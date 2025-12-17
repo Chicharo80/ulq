9fa82bc411d3e3bcb7b19e57fdc051b8f9d4634e
Chicharo80 <151591334+Chicharo80@users.noreply.github.com>
2025-12-17 13:09:24 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,2 @@
-const CACHE = "visitasulq-v4";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  // ❌ NO interceptar llamadas a Google Apps Script
-  if (e.request.url.includes("google.com")) {
-    return;
-  }
-
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("fetch", e => {});
